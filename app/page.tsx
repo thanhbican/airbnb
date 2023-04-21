f47156fdfbce0d9ab6c1555adf083bac8f3dd77e
FASTCODING SAM <sam.fastcodingvn@gmail.com>
2023-04-21 12:01:13 +0700

    fixup

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,7 @@ import ClientOnly from './components/ClientOnly';
 import Container from './components/Container';
 import EmptyState from './components/EmptyState';
 import ListingCard from './components/listings/ListingCard';
-
+export const dynamic = 'force-dynamic';
 interface HomeProps {
   searchParams: IListingsParams;
 }
@@ -16,7 +16,7 @@ const Home = async ({ searchParams }: HomeProps) => {
   if (!listings.length) {
     return (
       <ClientOnly>
-        <EmptyState showReset/>
+        <EmptyState showReset />
       </ClientOnly>
     );
   }
